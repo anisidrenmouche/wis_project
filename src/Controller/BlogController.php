e3af24d83aa28b3c774fa29b786d48bd30eefbe9
mrlaowilly <willy.meksavanh@hotmail.fr>
2019-10-01 17:34:48 +0200

    phase2

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -6,6 +6,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Article;
 use App\Repository\ArticleRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use App\Form\ArticleType;
 
 class BlogController extends AbstractController
 {
@@ -28,6 +33,37 @@ class BlogController extends AbstractController
     public function home() {
         return $this->render('blog/home.html.twig', [
             'title' => "Bienvenue ici les amis !"
+        ]);
+    }
+    
+    /**
+     * @Route("/blog/new", name="blog_create")
+     * @Route("/blog/{id}/edit", name="blog_edit")
+     */
+    public function form(Article $article = null, Request $request, ObjectManager $manager) {
+        
+        if(!$article) {
+            $article = new Article(); 
+        }
+        
+        $form = $this->createForm(ArticleType::class, $article);
+
+        $form->handleRequest($request);
+        
+        if($form->isSubmitted() && $form->isValid()) {
+            if(!$article->getId()){
+                $article->setCreatedAt(new \DateTime());
+            }
+
+            $manager->persist($article);
+            $manager->flush();
+
+            return $this->redirectToRoute('blog_show', ['id' => $article->getId()]);
+        }
+
+        return $this->render('blog/create.html.twig', [
+            'formArticle' => $form->createView(),
+            'editMode' => $article->getId() !== null
         ]);
     }
 
